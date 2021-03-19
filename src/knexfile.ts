108320752426eaf6e32dce3368f659fba0bc5d88
Adam L Edwards <adam@aeweb.io>
2021-03-19 07:11:29 +0000

    update

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -2,8 +2,6 @@ require("dotenv").config();
 
 module.exports = {
   client: "postgresql",
-  connectionString: process.env.DATABASE_URL, 
-  ssl: { 
-      rejectUnauthorized: false 
-  } 
+  connection: process.env.DATABASE_URL,
+  ssl: true
 }
