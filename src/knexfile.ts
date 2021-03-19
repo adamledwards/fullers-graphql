c98be7cf7991ad0ad424cabd300fcbe53199a4fb
Adam L Edwards <adam@aeweb.io>
2021-03-19 21:32:00 +0000

    ssl retry

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,7 +1,8 @@
 require("dotenv").config();
-
+const {parse} = require('pg-connection-string')
+const pgconfig = parse(process.env.DATABASE_URL);
+pgconfig.ssl = { rejectUnauthorized: false };
 module.exports = {
   client: "postgresql",
-  connection: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  connection: pgconfig,
 }
